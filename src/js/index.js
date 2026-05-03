3f19cfc020b88bd189ebe2f85f8f5581d1dd1b01
Chleih <Chleih@outlook.com>
2026-05-03 13:39:43 +0200

    Change root index to become an entry and create folders for pages.

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,23 @@
+import homePageTemplate from '../html/pages/home.html?raw';
 import '../scss/main.scss';
 import { initLanguage } from './i18n';
 import { initUi } from './ui';
 
 /**
+ * Renders the landing page into the root application entry point.
+ */
+function renderHomePage() {
+    const applicationRoot = document.getElementById('app');
+
+    applicationRoot.innerHTML = homePageTemplate;
+}
+
+/**
  * Initializes the application after the DOM is ready.
  */
 async function initializeApplication() {
+    renderHomePage();
+
     await initLanguage();
     initUi();
 }
