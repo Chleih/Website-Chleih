77658e5fe2d23e5278671e505bd3a35a3ad9ce93
Chleih <Chleih@outlook.com>
2026-05-03 18:31:09 +0200

    Cleanup code.

--- src/js/ui/index.js
+++ src/js/ui/index.js
@@ -1,9 +1,7 @@
 import { initBrandHover } from './init-brand-hover';
 import { initLoader } from './init-loader';
-import { initParticles } from './init-particles';
 
 export function initUi() {
     initBrandHover();
     initLoader();
-    initParticles();
 }
