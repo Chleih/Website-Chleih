5480cb8475d172306dd1623506515956be57f63e
Chleih <Chleih@outlook.com>
2026-05-07 22:15:32 +0200

    Implement root 404 for application.

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,16 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'node:path';
+
+const htmlEntries = {
+    main: resolve(process.cwd(), 'index.html'),
+    notFound: resolve(process.cwd(), '404.html'),
+};
 
 export default defineConfig({
     base: '/',
+    build: {
+        rollupOptions: {
+            input: htmlEntries,
+        },
+    },
 });
