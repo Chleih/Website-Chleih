cdd486b640e5a75686050543f040bdbcf13fa083
Chleih <Chleih@outlook.com>
2026-05-09 21:39:47 +0200

    Added route page definitions.

---
+++ src/js/router/page-definitions.js
@@ -0,0 +1,24 @@
+export const NOT_FOUND_PAGE_NAME = 'not-found';
+
+export const appPageDefinitions = [
+    {
+        name: 'home',
+        path: '/',
+    },
+    {
+        name: 'about',
+        path: '/about',
+    },
+    {
+        name: 'work',
+        path: '/work',
+    },
+    {
+        name: 'stack',
+        path: '/stack',
+    },
+    {
+        name: 'contact',
+        path: '/contact',
+    },
+];
